e6339efd7692c46f52f6cf484ec323ca05b0a170
St3ph4n1e <boisne.stephanie@edu.esiee-it.fr>
2024-06-27 17:56:32 +0200

    change Title and logo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,42 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 
-defineProps<{ msg: string }>()
-
-const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+	<div class="flex flex-col gap-8 items-center">
+		<div class="w-full">
+			<h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-black md:text-5xl lg:text-6xl"><span
+					class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Coacheta</span>
+				Your Fitness Assistant</h1>
+		</div>
+		<div class="mt-4 flex w-full">
+			<div class="flex-[1_1_100%]">
+				<img src="../assets/imgs/Creation_de_seance.png" />
+			</div>
+			<div class="card flex-[1_1_100%]">
+				<div class="text-3xl font-extrabold text-gray-900">Ecran d'accueil</div>
+				<div class="flex flex-col items-start gap-4 ">
+					<div class="text-l break-words text-left">- Accéder à la page Mes séances et Programmes </div>
+					<div class="text-l break-words text-left">- Accéder à la page de suivi de progression : statistiques </div>
+					<div class="text-l break-words text-left">- Affichage du calendrier à la date du jour : ainsi que la séance ou évent prévu ce jour </div>
+					<div class="text-l break-words text-left">- Possibilité d’ajouter une séance déjà existante  à une date </div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
