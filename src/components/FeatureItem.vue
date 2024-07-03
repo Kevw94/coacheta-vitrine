3e4be6eb10990e75bcafdc1039aae5eb5ef2715d
St3ph4n1e <boisne.stephanie@edu.esiee-it.fr>
2024-07-03 14:08:55 +0200

    responsive feature page

--- src/components/FeatureItem.vue
+++ src/components/FeatureItem.vue
@@ -8,11 +8,9 @@
 <script setup lang="ts">
 import {getUrl} from "../shared/utils.ts";
 
-
 defineProps<{
   image: string ;
   text: string;
-
 }>();
 </script>
 
@@ -22,6 +20,4 @@ defineProps<{
   background: rgba(255, 255, 255, 0.10);
   box-shadow: 0 8px 12px 6px rgba(0, 0, 0, 0.15), 0 4px 4px 0 rgba(0, 0, 0, 0.30);
 }
-
-
 </style>
