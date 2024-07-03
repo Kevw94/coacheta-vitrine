b9fa847c892a635a015d94ace4a3dc63805745fb
St3ph4n1e <boisne.stephanie@edu.esiee-it.fr>
2024-07-03 13:31:00 +0200

    responsive header and about page

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,15 +6,30 @@
         <h1 class="text-2xl font-bold">Coacheta</h1>
       </router-link>
     </div>
-    <nav>
-      <ul class="flex space-x-8">
+    <nav :class="{'hidden': !isMenuOpen}" class="md:flex space-x-8">
+      <ul class="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-8 mt-4 md:mt-0">
         <li><router-link to="/" class="nav-link">Home</router-link></li>
         <li><router-link to="/features" class="nav-link">Features</router-link></li>
         <li><router-link to="/about" class="nav-link">About</router-link></li>
       </ul>
     </nav>
+    <button @click="toggleMenu" class="block md:hidden">
+      <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+      </svg>
+    </button>
   </header>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const isMenuOpen = ref(false);
+
+const toggleMenu = () => {
+  isMenuOpen.value = !isMenuOpen.value;
+};
+</script>
 
 <style scoped>
 .text-gradient {
@@ -34,5 +49,17 @@
 
 .nav-link:hover {
   color: #9EFFF0;
+}
+
+@media (max-width: 768px) {
+  nav {
+    display: none;
+  }
+  nav.hidden {
+    display: flex;
+  }
+  nav ul {
+    flex-direction: column;
+  }
 }
 </style>
